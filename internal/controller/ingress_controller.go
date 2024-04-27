902da1233120e51b35904845c6e691aad4f20ce5
Arul <arulprasath73@outlook.com>
2024-04-27 10:19:11 +0530

    Added Finalizer for Route53Pilot

--- internal/controller/ingress_controller.go
+++ internal/controller/ingress_controller.go
@@ -25,15 +25,47 @@ type IngressReconciler struct {
 //+kubebuilder:rbac:groups=networking.k8s.io.route53pilot,resources=ingresses/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=networking.k8s.io.route53pilot,resources=ingresses/finalizers,verbs=update
 
+func containsString(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
+
+func removeString(slice []string, s string) []string {
+	for i, v := range slice {
+		if v == s {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
+}
+
 func (r *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
 	var ingress networkingv1.Ingress
 	if err := r.Get(ctx, req.NamespacedName, &ingress); err != nil {
+		log.Error(err, "Failed to get Ingress")
+		// log.Info(ingress.GetDeletionTimestamp().GoString())
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	// Check if the Ingress is being deleted
+	if !ingress.GetDeletionTimestamp().IsZero() {
+		// Ingress is in the process of being deleted
+		annotations := ingress.GetAnnotations()
+		log.Info("Ingress is being deleted", "annotations", annotations)
+
+		// Example: Process or log a specific annotation
+		if value, exists := annotations["your-annotation-key"]; exists {
+			log.Info("Annotation value", "your-annotation-key", value)
+		}
+	}
 	// TODO Add validation for required annotations
+	// TODO Handle wait for hostname
 
 	// Fetch domain name from annotation
 	domainName := ingress.Annotations["route53.kubernetes.io/domain-name"]
@@ -80,15 +112,33 @@ func (r *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 	action := "UPSERT"
 	if !ingress.ObjectMeta.DeletionTimestamp.IsZero() {
+		log.Info("Ingress is being deleted")
 		action = "DELETE"
 	}
 
+	if ingress.ObjectMeta.DeletionTimestamp.IsZero() && !containsString(ingress.ObjectMeta.Finalizers, "route53pilot/finalizer") {
+		ingress.ObjectMeta.Finalizers = append(ingress.ObjectMeta.Finalizers, "route53pilot/finalizer")
+		if err := r.Update(ctx, &ingress); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
 	recordName := subDomainName + "." + domainName
+
 	if err := r.createOrUpdateDNSRecord(r53, hostedZoneID, recordName, lbIP, action); err != nil {
 		log.Error(err, "Failed to manage Route 53 record")
 		return ctrl.Result{}, err
 	}
 
+	if !ingress.ObjectMeta.DeletionTimestamp.IsZero() {
+		ingress.ObjectMeta.Finalizers = removeString(ingress.ObjectMeta.Finalizers, "route53pilot/finalizer")
+		if err := r.Update(ctx, &ingress); err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+
+	log.Info("Successfully managed Route 53 record")
 	return ctrl.Result{}, nil
 }
 
